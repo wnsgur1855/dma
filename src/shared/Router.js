8cec9e54d14207c6352dcd2fa1cc52a7df39474b
DESKTOP-LTPLH83\USER <wnsgur1855@naver.com>
2024-02-21 20:19:21 +0900

    [style] | login component

---
+++ src/shared/Router.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from '../pages/Main';
+import Login from '../pages/Login';
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
