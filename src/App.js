f0d9593fd4f6c2f1263fca59fb8c599dddce5c73
DESKTOP-LTPLH83\USER <wnsgur1855@naver.com>
2024-02-29 21:26:46 +0900

    [feat]  | 로그인 폼

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import Router from './shared/Router';
+import { QueryClient } from 'react-query';
+import { QueryClientProvider } from 'react-query';
 
 function App() {
-  return <Router />;
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
--- src/components/Main/Memo.jsx
+++ src/components/Main/Memo.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { useQuery } from 'react-query';
+import axios from 'axios';
 import styled from 'styled-components';
 import MemoCRUD from './MemoCRUD';
 import MemoPage from './MemoPage';
@@ -11,34 +13,109 @@ const MemoContainer = styled.div`
   width: 400px;
   height: 500px;
   margin: 50px 0 0 20px;
+  background-color: black;
 `;
 
-const MemoTextarea = styled.textarea`
-  width: 98.5%;
-  height: 80%;
-  display: flex;
-  justify-content: center;
-  font-family: 'Nanum Gothic', sans-serif;
-  background-color: rgba(94, 91, 91, 0.219);
-  outline: none;
-  text-align: left;
+const TodoListBox = styled.div`
+  width: 100%;
+  height: 300px;
+  background-color: red;
 `;
+
+const TodoList = styled.ul`
+  width: 100%;
+  padding: 0;
+  list-style-type: none;
+`;
+
+const TodoItem = styled.li`
+  display: flex;
+  align-items: center;
+  margin-bottom: 5px;
+`;
+
+const AddButton = styled.button`
+  border: none;
+  background: none;
+  cursor: pointer;
+  margin-right: 10px;
+`;
+
+const TodoInput = styled.input`
+  width: calc(100% - 30px);
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 5px;
+`;
+
+const fetchData = async (date) => {
+  const formattedDate = date.toISOString().split('T')[0];
+  const response = await axios.get(
+    `http://localhost:8080/api/events?createdAt=${formattedDate}`
+  );
+  return response.data;
+};
 
 const Memo = ({ selectedDate }) => {
-  const [memo, setMemo] = useState('');
+  const [inputValue, setInputValue] = useState('');
+  const [showInput, setShowInput] = useState(false);
 
-  const handleMemoChange = (e) => {
-    setMemo(e.target.value);
+  const {
+    data: todos,
+    isLoading,
+    isError,
+  } = useQuery(['events', selectedDate], () => fetchData(selectedDate), {
+    enabled: selectedDate !== null, // 선택된 날짜가 없는 경우에는 쿼리를 실행하지 않음
+  });
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
   };
 
-  const handleSaveMemo = () => {
-    console.log(`날짜 ${selectedDate}에 메모 저장: ${memo}`);
+  const handleInputKeyDown = (e) => {
+    if (e.key === 'Enter' && inputValue.trim() !== '') {
+      // 실제로 서버로의 데이터 추가 요청이 필요하면 여기서 axios.post 등을 사용하여 요청을 보냄
+      setInputValue('');
+      setShowInput(false);
+    }
   };
+
+  const handleAddButtonClick = () => {
+    setShowInput(true);
+  };
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error fetching data</div>;
 
   return (
     <MemoContainer>
-      <MemoTextarea value={memo} onChange={handleMemoChange} />
-      {/* <button onClick={handleSaveMemo}>저장</button> */}
+      <TodoListBox>
+        <TodoList>
+          {todos.map((todo, index) => (
+            <TodoItem key={index}>
+              <span>
+                {index + 1}.{todo.title}
+              </span>
+            </TodoItem>
+          ))}
+          {showInput && (
+            <TodoItem>
+              <TodoInput
+                type='text'
+                placeholder='Add a new todo'
+                value={inputValue}
+                onChange={handleInputChange}
+                onKeyDown={handleInputKeyDown}
+              />
+            </TodoItem>
+          )}
+          {!showInput && (
+            <TodoItem>
+              <AddButton onClick={handleAddButtonClick}>+</AddButton>
+            </TodoItem>
+          )}
+        </TodoList>
+      </TodoListBox>
       <MemoPage />
       <MemoCRUD />
     </MemoContainer>
